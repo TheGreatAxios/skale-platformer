638da0e17b0c56369d301f8862f702ad8b4695df
TheGreatAxios <sawyer@dirtroad.dev>
2023-06-23 15:35:44 -0700

    Add Level 5

--- game/resources.ts
+++ game/resources.ts
@@ -25,6 +25,7 @@ const resources = [
     { name: "map2",            type: "tmx",    src: "./data/map/map2.json" },
     { name: "map3",            type: "tmx",    src: "./data/map/map3.json" },
     { name: "map4",            type: "tmx",    src: "./data/map/map4.json" },
+    { name: "map4",            type: "tmx",    src: "./data/map/map5.json" },
 
 
     /* Tilesets.
